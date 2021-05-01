a00b8fdfdba9dffb87d568009af56af52cedef34
Mark Lin <marklin@Lenny.local>
2021-05-02 02:01:46 +0800

    refactor: 重命名 tag 定义

--- lib/st_tag/common.dart
+++ lib/st_tag/common.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 
 enum STTagType {
-  normal,
+  primary,
   success,
-  failure,
+  error,
   warning,
 }
 
-enum STTagCorner { normal, circle, rightHalf }
-enum STTagTextSizeType { big, normal, small }
+enum STTagShape {
+  primary, // 主色
+  outline, // 空心边框
+  roundRect, // 圆角
+  rightRoundRect, // 右侧圆角
+}
+enum STTagSize {
+  big,
+  normal,
+  small,
+}
 
 class STTagConstant {
-  static const colorNormal = Color(0xFF095BF9);
-  static const colorSuccess = Color(0xFF49C564);
-  static const colorFailure = Color(0xFFFF4141);
-  static const colorWarnning = Color(0xFFFFA927);
+  static const Color colorPrimary = Color(0xFF095BF9);
+  static const Color colorSuccess = Color(0xFF49C564);
+  static const Color colorError = Color(0xFFFF4141);
+  static const Color colorWarnning = Color(0xFFFFA927);
+  static const double bigTextFont = 18.0;
+  static const double normalTextFont = 16.0;
+  static const double smallTextFont = 14.0;
 }
