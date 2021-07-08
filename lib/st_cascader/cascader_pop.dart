5f05ac65c7a7c45a14b405c4af3a415f134b31c7
Harry <MingChenXX@163.com>
2021-07-09 00:34:23 +0800

    ref: cascader 验收问题修改

--- lib/st_cascader/cascader_pop.dart
+++ lib/st_cascader/cascader_pop.dart
@@ -8,6 +8,14 @@ const _defaultTextStyle = TextStyle(color: _defaultColor, fontSize: 14);
 const _defaultIcon =
     Icon(STIcons.direction_rightoutlined, size: 16, color: _defaultColor);
 const _defaultMargin = EdgeInsets.symmetric(horizontal: 16.0);
+const _leftBorderDecoration = BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.horizontal(left: Radius.circular(8.0)),
+);
+const _rightBorderDecoration = BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.horizontal(right: Radius.circular(8.0)),
+);
 
 class STCascaderPopItem {
   final String value;
@@ -31,9 +39,9 @@ class STCascaderPop extends StatefulWidget {
 }
 
 class _STCascaderPopState extends State<STCascaderPop> {
+  final List<int> _selectedIndexs = []..length = 3;
   final _maxColumn = 3;
   double _columnWidth;
-  final List<int> _selectedIndexs = []..length = 3;
 
   @override
   Widget build(BuildContext context) {
@@ -44,17 +52,14 @@ class _STCascaderPopState extends State<STCascaderPop> {
     return Container(
       margin: _defaultMargin,
       decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(8.0)),
         boxShadow: [
           BoxShadow(
-            blurRadius: 2,
-            spreadRadius: 1,
+            blurRadius: 5,
             color: Color(0x19000000),
-            offset: Offset(1, 2),
-          ),
+          )
         ],
       ),
+      // child: Text('data'),
       child: Row(
         children: [
           _getListWidget(0),
@@ -66,28 +71,47 @@ class _STCascaderPopState extends State<STCascaderPop> {
   }
 
   Widget _getListWidget(int column) {
-    var _decoration = const BoxDecoration();
     List<STCascaderPopItem> _items;
+    var _decoration = const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+    );
     if (column == 0) {
       _items = _getColumnValues(widget.items);
-      _decoration = const BoxDecoration(
-        borderRadius: BorderRadius.horizontal(left: Radius.circular(8.0)),
-      );
+      if (_selectedIndexs[0] != null) {
+        final _selectedIndex = _selectedIndexs[0];
+        final _subItem = _items[_selectedIndex];
+        if (_subItem.next) {
+          _decoration = _leftBorderDecoration;
+        }
+      }
     } else if (column == 1) {
       final _temp = widget.items[_selectedIndexs[0]].items;
       _items = _getColumnValues(_temp);
+      if (_selectedIndexs[1] != null) {
+        final _selectedIndex = _selectedIndexs[1];
+        final _subItem = _items[_selectedIndex];
+        if (_subItem.next) {
+          _decoration = const BoxDecoration(
+            color: Colors.white,
+          );
+        } else {
+          _decoration = _rightBorderDecoration;
+        }
+      } else {
+        _decoration = _rightBorderDecoration;
+      }
     } else if (column == 2) {
       final _temp =
           widget.items[_selectedIndexs[0]].items[_selectedIndexs[1]].items;
       _items = _getColumnValues(_temp);
-      _decoration = const BoxDecoration(
-        borderRadius: BorderRadius.horizontal(right: Radius.circular(8.0)),
-      );
+      _decoration = _rightBorderDecoration;
     }
     if (_items == null || _items.isEmpty) return Container();
     return Container(
-      width: _columnWidth,
       decoration: _decoration,
+      width: _columnWidth,
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: ListView.builder(
         itemCount: _items.length,
         itemExtent: 44.0,
