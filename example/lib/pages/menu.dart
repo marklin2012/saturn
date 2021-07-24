b03d932eb4931bf3a0b6b49a61930919be8fb3e2
Mark Lin <marklin@Lenny.lan>
2021-07-25 07:51:53 +0800

    refactor: 优化menu 展示样式

--- example/lib/pages/menu.dart
+++ example/lib/pages/menu.dart
@@ -7,74 +7,154 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
-  int _current = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            STMenu(
-              onTap: (index) {
-                print('切换到第$index个内容');
-              },
-              items: [
-                STMenuDataItem(title: '文本1'),
-                STMenuDataItem(title: '文本2'),
-                STMenuDataItem(title: '文本3'),
-                STMenuDataItem(title: '文本4'),
-                STMenuDataItem(title: '文本5'),
-                // STMenuDataItem(title: '文本6'),
-                // STMenuDataItem(title: '文本7'),
-              ],
-            ),
-            STMenu(
-              type: STMenuType.underline,
-              onTap: (index) {
-                print('切换到第$index个内容');
-              },
-              items: [
-                STMenuDataItem(title: '文本1'),
-                STMenuDataItem(title: '文本2'),
-                STMenuDataItem(title: '文本3'),
-                STMenuDataItem(title: '文本4'),
-                STMenuDataItem(title: '文本5'),
-                // STMenuDataItem(title: '文本6'),
-                // STMenuDataItem(title: '文本7'),
-              ],
-            ),
-            STMenu(
-              type: STMenuType.section,
-              onTap: (index) {
-                print('切换到第$index个内容');
-              },
-              items: [
-                STMenuDataItem(title: '文本1'),
-                STMenuDataItem(title: '文本2'),
-                STMenuDataItem(title: '文本3'),
-                STMenuDataItem(title: '文本4'),
-                STMenuDataItem(title: '文本5'),
-                // STMenuDataItem(title: '文本6'),
-                // STMenuDataItem(title: '文本7'),
-              ],
-            ),
-            STMenu(
-              type: STMenuType.label,
-              onTap: (index) {
-                print('切换到第$index个内容');
-              },
-              items: [
-                STMenuDataItem(title: '文本1', icon: Icons.home),
-                STMenuDataItem(title: '文本2', icon: Icons.favorite),
-                STMenuDataItem(title: '文本3', icon: Icons.history),
-                STMenuDataItem(title: '文本4', icon: STIcons.commonly_wallet),
-                STMenuDataItem(title: '文本5', icon: Icons.settings),
-                // STMenuDataItem(title: '文本6'),
-                // STMenuDataItem(title: '文本7'),
-              ],
-            ),
-          ],
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu'),
+      ),
+      body: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, top: 24, bottom: 16, right: 8),
+                child: Text(
+                  '基本用法',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                ),
+              ),
+              STMenu(
+                onTap: (index) {
+                  print('切换到第$index个内容');
+                },
+                items: [
+                  STMenuDataItem(title: '文本1'),
+                  STMenuDataItem(title: '文本2'),
+                  STMenuDataItem(title: '文本3'),
+                  STMenuDataItem(title: '文本4'),
+                  STMenuDataItem(title: '文本5'),
+                  // STMenuDataItem(title: '文本6'),
+                  // STMenuDataItem(title: '文本7'),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, top: 24, bottom: 16, right: 8),
+                child: Text(
+                  '下划线样式',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                ),
+              ),
+              STMenu(
+                type: STMenuType.underline,
+                onTap: (index) {
+                  print('切换到第$index个内容');
+                },
+                items: [
+                  STMenuDataItem(title: '文本1'),
+                  STMenuDataItem(title: '文本2'),
+                  STMenuDataItem(title: '文本3'),
+                  STMenuDataItem(title: '文本4'),
+                  STMenuDataItem(title: '文本5'),
+                  // STMenuDataItem(title: '文本6'),
+                  // STMenuDataItem(title: '文本7'),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, top: 24, bottom: 16, right: 8),
+                child: Text(
+                  '分段样式',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                ),
+              ),
+              STMenu(
+                type: STMenuType.section,
+                onTap: (index) {
+                  print('切换到第$index个内容');
+                },
+                items: [
+                  STMenuDataItem(title: '文本1'),
+                  STMenuDataItem(title: '文本2'),
+                  STMenuDataItem(title: '文本3'),
+                  STMenuDataItem(title: '文本4'),
+                  STMenuDataItem(title: '文本5'),
+                  // STMenuDataItem(title: '文本6'),
+                  // STMenuDataItem(title: '文本7'),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, top: 24, bottom: 16, right: 8),
+                child: Text(
+                  '滚动样式',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                ),
+              ),
+              STMenu(
+                type: STMenuType.section,
+                onTap: (index) {
+                  print('切换到第$index个内容');
+                },
+                items: [
+                  STMenuDataItem(title: '文本1'),
+                  STMenuDataItem(title: '文本2'),
+                  STMenuDataItem(title: '文本3'),
+                  STMenuDataItem(title: '文本4'),
+                  STMenuDataItem(title: '文本5'),
+                  STMenuDataItem(title: '文本6'),
+                  STMenuDataItem(title: '文本7'),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, top: 24, bottom: 16, right: 8),
+                child: Text(
+                  '标签样式',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                ),
+              ),
+              STMenu(
+                type: STMenuType.label,
+                onTap: (index) {
+                  print('切换到第$index个内容');
+                },
+                items: [
+                  STMenuDataItem(title: '文本1', icon: Icons.home),
+                  STMenuDataItem(title: '文本2', icon: Icons.favorite),
+                  STMenuDataItem(title: '文本3', icon: Icons.history),
+                  STMenuDataItem(title: '文本4', icon: STIcons.commonly_wallet),
+                  STMenuDataItem(title: '文本5', icon: Icons.settings),
+                  // STMenuDataItem(title: '文本6'),
+                  // STMenuDataItem(title: '文本7'),
+                ],
+              ),
+            ],
+          )),
+    );
   }
 }
