34cc6149a0687cd9c42c80ea18b15e206616a7a9
Harry <MingChenXX@163.com>
2021-10-16 23:22:24 +0800

    feat:添加input用例

--- example/lib/pages/input.dart
+++ example/lib/pages/input.dart
@@ -8,46 +8,53 @@ class InputPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Inputs'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: STInput(
-                cursorColor: Colors.red,
-                prefixIcon: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    STButton(
-                      type: STButtonType.text,
-                      text: '+86',
-                      onTap: () {},
-                    ),
-                    Icon(
-                      STIcons.direction_caretdown,
-                      color: Colors.black,
-                    ),
-                  ],
-                ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: STInput(
+              cursorColor: Colors.red,
+              prefixIcon: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  STButton(
+                    type: STButtonType.text,
+                    text: '+86',
+                    onTap: () {},
+                  ),
+                  Icon(
+                    STIcons.direction_caretdown,
+                    color: Colors.black,
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: STInput.password(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: STInput(
-                suffixIcon: STButton(
-                  text: '获取验证码',
-                  onTap: () {},
-                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: STInput.password(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: STInput(
+              suffixIcon: STButton(
+                text: '获取验证码',
+                onTap: () {},
               ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: STInput(
+              height: 32,
+              placeholder: '请输入账号',
+              cursorHeight: 24,
+              contentPadding: EdgeInsets.only(bottom: 16),
+            ),
+          ),
+        ],
       ),
     );
   }
