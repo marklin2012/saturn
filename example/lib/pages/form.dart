9bf8abcd061726c37209ff68a4f000bc513f1801
Harry <MingChenXX@163.com>
2021-12-17 12:31:50 +0800

    feat: 修改form排版

--- example/lib/pages/form.dart
+++ example/lib/pages/form.dart
@@ -4,6 +4,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:saturn/saturn.dart';
 
+const tipTextStyle = TextStyle(
+  fontSize: 16,
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+);
+
 class FormPage extends StatefulWidget {
   const FormPage({Key key}) : super(key: key);
 
@@ -51,14 +57,14 @@ class _FormPageState extends State<FormPage> {
     return Column(
       children: [
         STRowOption(
-          leading: Text('头像'),
-          trailing: STImage.avatar(iconWidth: 32),
+          leading: Text('头像', style: tipTextStyle),
+          trailing: STImage.avatar(width: 32),
           isHavRightArrow: true,
         ),
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('姓名'),
+            child: Text('姓名', style: tipTextStyle),
           ),
           center: STFormInput(
             placeholder: '必填',
@@ -67,6 +73,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _name = value;
+              debugPrint(_name);
             },
             validator: (String value) {
               return value.length >= 2 ? '姓名验证成功' : '姓名不可少于2个字符';
@@ -76,7 +83,7 @@ class _FormPageState extends State<FormPage> {
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('性别'),
+            child: Text('性别', style: tipTextStyle),
           ),
           center: STFormInput(
             placeholder: '必填',
@@ -85,6 +92,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _sex = value;
+              debugPrint(_sex);
             },
             validator: (String value) {
               return value.length >= 1 ? '性别验证成功' : '性别不可少于1个字符';
@@ -95,7 +103,7 @@ class _FormPageState extends State<FormPage> {
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('用户昵称'),
+            child: Text('用户昵称', style: tipTextStyle),
           ),
           center: STFormInput(
             placeholder: '必填',
@@ -104,6 +112,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _nick = value;
+              debugPrint(_nick);
             },
             validator: (String value) {
               return value.length >= 6 ? '昵称验证成功' : '昵称不可少于6个字符';
@@ -114,7 +123,7 @@ class _FormPageState extends State<FormPage> {
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('出生年月'),
+            child: Text('出生年月', style: tipTextStyle),
           ),
           trailing: STButton(
             type: STButtonType.text,
@@ -141,19 +150,19 @@ class _FormPageState extends State<FormPage> {
           isHavRightArrow: true,
           isHavBottomLine: false,
         ),
-        Padding(
-          padding: EdgeInsets.only(top: 10, left: 16, right: 16),
-          child: STButton(
-            text: '登录',
-            mainAxisSize: MainAxisSize.max,
-            onTap: () {
-              final _state = _formFirstKey.currentState;
-              if (_state.validate()) {
-                _state.save();
-              }
-            },
-          ),
-        )
+        // Padding(
+        //   padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+        //   child: STButton(
+        //     text: '登录',
+        //     mainAxisSize: MainAxisSize.max,
+        //     onTap: () {
+        //       final _state = _formFirstKey.currentState;
+        //       if (_state.validate()) {
+        //         _state.save();
+        //       }
+        //     },
+        //   ),
+        // )
       ],
     );
   }
@@ -170,6 +179,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _address = value;
+              debugPrint(_address);
             },
             validator: (String value) {
               return value.length >= 12 ? null : '地址长度不低于12位';
@@ -185,6 +195,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _id = value;
+              debugPrint(_id);
             },
             validator: (String value) {
               return value.length >= 18 ? null : 'ID长度不低于18位';
@@ -200,6 +211,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _email = value;
+              debugPrint(_email);
             },
             validator: (String value) {
               return value.length >= 10 ? null : '邮箱长度不低于10位';
@@ -207,19 +219,19 @@ class _FormPageState extends State<FormPage> {
           ),
           isHavBottomLine: false,
         ),
-        Padding(
-          padding: EdgeInsets.only(top: 10, left: 16, right: 16),
-          child: STButton(
-            text: '保存',
-            mainAxisSize: MainAxisSize.max,
-            onTap: () {
-              final _state = _formSecondKey.currentState;
-              if (_state.validate()) {
-                _state.save();
-              }
-            },
-          ),
-        )
+        // Padding(
+        //   padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+        //   child: STButton(
+        //     text: '保存',
+        //     mainAxisSize: MainAxisSize.max,
+        //     onTap: () {
+        //       final _state = _formSecondKey.currentState;
+        //       if (_state.validate()) {
+        //         _state.save();
+        //       }
+        //     },
+        //   ),
+        // )
       ],
     );
   }
@@ -230,7 +242,7 @@ class _FormPageState extends State<FormPage> {
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('旧密码'),
+            child: Text('旧密码', style: tipTextStyle),
           ),
           center: STFormInput(
             placeholder: '请输入旧密码',
@@ -239,6 +251,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _oldPassword = value;
+              debugPrint(_oldPassword);
             },
             validator: (String value) {
               return value.length >= 10 ? null : '密码不可少于10个字符';
@@ -248,7 +261,7 @@ class _FormPageState extends State<FormPage> {
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('新密码'),
+            child: Text('新密码', style: tipTextStyle),
           ),
           center: STFormInput.password(
             placeholder: '请设置新密码',
@@ -257,6 +270,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _newPassword = value;
+              debugPrint(_newPassword);
             },
             validator: (String value) {
               return value.length >= 10 ? null : '密码不可少于10个字符';
@@ -266,7 +280,7 @@ class _FormPageState extends State<FormPage> {
         STRowOption(
           leading: Container(
             width: 64,
-            child: Text('确认密码'),
+            child: Text('确认密码', style: tipTextStyle),
           ),
           center: STFormInput.password(
             placeholder: '请再次输入新密码',
@@ -275,6 +289,7 @@ class _FormPageState extends State<FormPage> {
             ],
             onSaved: (value) {
               _conPassword = value;
+              debugPrint(_conPassword);
             },
             validator: (String value) {
               return value.length >= 10 ? null : '密码不可少于10个字符';
@@ -282,19 +297,19 @@ class _FormPageState extends State<FormPage> {
           ),
           isHavBottomLine: false,
         ),
-        Padding(
-          padding: EdgeInsets.only(top: 10, left: 16, right: 16),
-          child: STButton(
-            text: '修改',
-            mainAxisSize: MainAxisSize.max,
-            onTap: () {
-              final _state = _formThreeKey.currentState;
-              if (_state.validate()) {
-                _state.save();
-              }
-            },
-          ),
-        )
+        // Padding(
+        //   padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+        //   child: STButton(
+        //     text: '修改',
+        //     mainAxisSize: MainAxisSize.max,
+        //     onTap: () {
+        //       final _state = _formThreeKey.currentState;
+        //       if (_state.validate()) {
+        //         _state.save();
+        //       }
+        //     },
+        //   ),
+        // )
       ],
     );
   }
